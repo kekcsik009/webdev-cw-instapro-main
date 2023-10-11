59e92b3f734d926c02c9b3f46358a55ff904f465
Даниил <chirun1999@mail.ru>
2023-10-11 17:45:03 +0700

    Initial commit

---
+++ webdev-cw-instapro-main/components/posts-page-component.js
@@ -0,0 +1,77 @@
+import { USER_POSTS_PAGE } from "../routes.js";
+import { renderHeaderComponent } from "./header-component.js";
+import { posts, goToPage, likeEventListener } from "../index.js";
+
+const sanitizeHtml = (htmlString) => {
+  return htmlString
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;");
+};
+
+export function renderPostsPageComponent({ appEl }) {
+  // TODO: реализовать рендер постов из api +
+  console.log("Актуальный список постов:", posts);
+
+  // Создаем константу, которая получает строку, содержащую разметку для каждого поста из массива posts
+  const postsHtml = posts.map((post, index) => {
+    return `<li class="post">
+    <div class="post-header" data-user-id="${post.id}">
+        <img src=${post.userImg} class="post-header__user-image">
+        <p class="post-header__user-name">${post.name}</p>
+    </div>
+    <div class="post-image-container">
+      <img class="post-image" src=${post.postImg}>
+    </div>
+    <div class="post-likes">
+      <button data-post-id="${
+        post.idPost
+      }" data-index="${index}" class="like-button">
+        <img src="${
+          post.isLiked
+            ? `./assets/images/like-active.svg`
+            : `./assets/images/like-not-active.svg`
+        }">
+      </button>
+      <p class="post-likes-text">
+        Нравится: <strong>${post.whoseLike ? `${post.whoseLike}` : `0`} ${
+      post.likes > 1 ? `и еще ${post.likes - 1}` : ``
+    }</strong>
+      </p>
+    </div>
+    <p class="post-text">
+      <span class="user-name">${post.name}</span>
+      ${sanitizeHtml(post.description)}
+    </p>
+    <p class="post-date">
+      19 минут назад
+    </p>
+  </li>`;
+  });
+  // Создаем константу appHtml, которая получает строку, содержащую разметку для всего компонента страницы постов
+  const appHtml = `
+    <div class="page-container">
+    <div class="header-container"></div>
+      <ul class="posts">
+        ${postsHtml}
+      </ul>
+      
+    </div>`;
+
+  appEl.innerHTML = appHtml;
+
+  renderHeaderComponent({
+    element: document.querySelector(".header-container"),
+  });
+
+  likeEventListener();
+  // Добавляем обработчики событий на кнопки лайков
+  for (let userEl of document.querySelectorAll(".post-header")) {
+    userEl.addEventListener("click", () => {
+      goToPage(USER_POSTS_PAGE, {
+        userId: userEl.dataset.userId,
+      });
+    });
+  }
+}
